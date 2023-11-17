3c146cf07ac554ca4835fa99bd33cf8bce2864fc
westwoong <westwoong>
2023-11-18 05:48:10 +0900

    Feat : 숫자 입력기능 추가

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,10 @@
+import * as readlineSync from 'readline-sync';
+
 export class App {
     async play() {
-        console.log('test....');
+        console.log('숫자 야구 게임을 시작합니다.');
+        const number = readlineSync.question('숫자를 입력해주세요 : ');
+        console.log(number);
     }
 }
 
