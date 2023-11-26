319deb4902a6cb31c919381bc57149423d3bb70d
westwoong <westwoong>
2023-11-27 03:18:26 +0900

    Feat : 1~9까지 랜덤값 세자리를 가져오는 함수 추가

--- src/App.ts
+++ src/App.ts
@@ -5,23 +5,18 @@ export class App {
         console.log('숫자 야구 게임을 시작합니다.');
         const number = readlineSync.question('숫자를 입력해주세요 : ');
         console.log(number);
-        const test = this.shuffleNumber([1, 2, 3, 4, 5]);
-        console.log(test);
-        console.log(test.length);
+        const computerNumber = await this.generateThreeRandomNumber();
+        console.log(computerNumber);
+    }
+
+    private async generateThreeRandomNumber() {
+        const numberRule = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        return this.shuffleNumber(numberRule).slice(0, 3);
     }
 
     private shuffleNumber(numberArray: number[]): number[] {
-        // i == 배열의 길이(5) -1  , 4 , i가 0보다 클때까지만 i의 값을 뺀다. 4번 돌아간다  ;
         for (let i = numberArray.length - 1; i > 0; i--) {
-            // 랜덤 변수 하나 4가 들어오면 4+1 5가된다 랜덤난수를 곱해서 소수점을 떨굼
-            console.log('-----i-----')
-            console.log(i);
-            console.log('-----i-----')
             const randomIndex = Math.floor(Math.random() * (i + 1));
-            console.log('-----R-----')
-            console.log(randomIndex);
-            console.log('-----R-----');
-            // i = 4 , random = 0 ,                          0 ,   4
             [numberArray[i], numberArray[randomIndex]] = [numberArray[randomIndex], numberArray[i]];
         }
         return numberArray;
