3b80aee285742d1cb028508f4132b98638e6f638
westwoong <westwoong>
2023-11-27 04:38:55 +0900

    Feat : 사용자 input값 validate 함수 추가
    정규표현식, Set인스턴스 사용
    
    Set 인스턴스는 객체에 요소(값)를 추가해서
    인스턴스의 사이즈를 .size로 확인한다
    그럼 숫자 3개를 넣으니까 사이즈는 3이고
    
    값을 forEach 로 순회해서 중복값을 확인한다
    만약 112를 넣으면 중복값이 있어서 1을 삭제해서 사이즈가 2가되어
    에러가 발생

--- src/App.ts
+++ src/App.ts
@@ -4,9 +4,15 @@ export class App {
     async play() {
         console.log('숫자 야구 게임을 시작합니다.');
         const number = readlineSync.question('숫자를 입력해주세요 : ');
-        console.log(number);
-        const computerNumber = await this.generateThreeRandomNumber();
-        console.log(computerNumber);
+
+        try {
+            this.validateInputNumber(number);
+            console.log(number);
+            const computerNumber = await this.generateThreeRandomNumber();
+            console.log(computerNumber);
+        } catch (error: any) {
+            console.error(error.message);
+        }
     }
 
     private async generateThreeRandomNumber() {
@@ -20,6 +26,13 @@ export class App {
             [numberArray[i], numberArray[randomIndex]] = [numberArray[randomIndex], numberArray[i]];
         }
         return numberArray;
+    }
+
+    private validateInputNumber(inputNumber: string) {
+        const validateRule = /^[1-9]{3}$/;
+        const uniqueNumber = new Set(inputNumber);
+        if (!validateRule.test(inputNumber)) throw new Error('유효하지 않은 입력, 1~9까지 서로 다른 3자리 수를 입력해주세요.');
+        if (uniqueNumber.size !== 3) throw new Error('중복된 숫자가 있습니다. 서로 다른 숫자를 입력하세요.');
     }
 }
 
