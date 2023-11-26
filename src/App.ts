877b6c3a62907aa1987af11a9d3b93bbdb978d34
westwoong <westwoong>
2023-11-27 06:23:04 +0900

    Feat : 게임 승리시 종료 추가

--- src/App.ts
+++ src/App.ts
@@ -3,30 +3,39 @@ import * as readlineSync from 'readline-sync';
 export class App {
     async play() {
         console.log('숫자 야구 게임을 시작합니다.');
-        //내가 입력한거
-        const userInputNumber = readlineSync.question('숫자를 입력해주세요 : ');
 
-        try {
-            this.validateInputNumber(userInputNumber);
-            //컴퓨터가 입력한거
-            const computerNumber = await this.generateThreeRandomNumber();
-            console.log(`컴퓨터 입력값 : ${computerNumber}`);
+        const computerNumber = await this.generateThreeRandomNumber();
+        let game = false;
 
-            const result = this.resultGameValue(userInputNumber, String(computerNumber));
-            console.log(result);
-            // console.log(computerNumber);
-        } catch (error: any) {
-            console.error(error.message);
+        while (!game) {
+            try {
+                const userInputNumber = readlineSync.question('숫자를 입력해주세요 : ');
+                this.validateInputNumber(userInputNumber);
+
+                // const computerNumber = 512;
+                // console.log(`컴퓨터 입력값 : ${computerNumber}`);
+
+                const result = this.resultGameValue(userInputNumber, String(computerNumber));
+                console.log(result);
+
+                if (result === '3스트라이크 0볼') {
+                    console.log(`${userInputNumber} 니가 입력한거`)
+                    console.log('니가 이김');
+                    game = true;
+                }
+            } catch (error: any) {
+                console.error(error.message);
+            }
         }
     }
 
     private async generateThreeRandomNumber() {
-        const numberRule = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        const numberRule = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         const test = this.shuffleNumber(numberRule).slice(0, 3);
         return parseInt(test.join(''));
     }
 
-    private shuffleNumber(numberArray: string[]): string[] {
+    private shuffleNumber(numberArray: number[]): number[] {
         for (let i = numberArray.length - 1; i > 0; i--) {
             const randomIndex = Math.floor(Math.random() * (i + 1));
             [numberArray[i], numberArray[randomIndex]] = [numberArray[randomIndex], numberArray[i]];
@@ -46,7 +55,6 @@ export class App {
         let strike = 0;
         let count = 0;
         for (let i = 0; i < 3; i++) {
-            //스트라이크 계산
             if (userInput[i] === computerInput[i]) {
                 strike++;
             }
