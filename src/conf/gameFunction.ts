3081312054405984ec08f28e16e5722c0868c2ea
westwoong <westwoong>
2023-11-29 02:19:37 +0900

    Refactor : resultGameValue함수 기능파일로 이동

--- src/conf/gameFunction.ts
+++ src/conf/gameFunction.ts
@@ -4,6 +4,23 @@ export function generateThreeRandomNumber(): number {
     return parseInt(shuffleNumbers.join(''));
 }
 
+export function resultGameValue(userInput: string, computerInput: string): string {
+    let strike = 0;
+    let count = 0;
+    for (let strikePosition = 0; strikePosition < 3; strikePosition++) {
+        if (userInput[strikePosition] === computerInput[strikePosition]) {
+            strike++;
+        }
+        for (let ballPosition = 0; ballPosition < 3; ballPosition++) {
+            if (userInput[strikePosition] == computerInput[ballPosition]) {
+                count++;
+            }
+        }
+    }
+    let ball = count - strike;
+    return `${strike}스트라이크 ${ball}볼`;
+}
+
 function shuffleNumber(numberArray: number[]): number[] {
     for (let index = numberArray.length - 1; index > 0; index--) {
         const randomIndex = Math.floor(Math.random() * (index + 1));
