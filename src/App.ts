25fc7f7eb3e8e4583e731ca9b00d048649961311
westwoong <westwoong>
2023-11-28 23:26:26 +0900

    Refactor : 게임 클리어 확인 여부 함수로 분리

--- src/App.ts
+++ src/App.ts
@@ -1,30 +1,26 @@
 import * as readlineSync from 'readline-sync';
 
 export class App {
-    computerNumber: number;
+    private computerNumber: number;
+    private game: boolean;
 
     constructor() {
         console.log('숫자 야구 게임을 시작합니다.');
         this.computerNumber = this.generateThreeRandomNumber();
+        this.game = true;
     }
 
-    async play() {
-        let game = true;
-
-        while (game) {
+    public play() {
+        while (this.game) {
             try {
                 const userInputNumber = readlineSync.question('숫자를 입력해주세요 : ');
                 this.validateInputNumber(userInputNumber);
-
                 console.log(`컴퓨터 입력값 : ${this.computerNumber}`);
 
                 const result = this.resultGameValue(userInputNumber, String(this.computerNumber));
                 console.log(result);
 
-                if (result === '3스트라이크 0볼') {
-                    console.log(`${userInputNumber} 니가 입력한거`);
-                    game = await this.isPlayAgain();
-                }
+                this.isGameClear(result);
 
             } catch (error: any) {
                 console.error(error.message);
@@ -32,10 +28,16 @@ export class App {
         }
     }
 
-    private async isPlayAgain(): Promise<boolean> {
-        const playAgainInput = readlineSync.question('니가 이김, 게임 다시할꺼? 할꺼면 1 입력, 안할꺼면 2입력 : ');
+    private isGameClear(result: string) {
+        if (result === '3스트라이크 0볼') {
+            this.game = this.isPlayAgain();
+        }
+    }
+
+    private isPlayAgain(): boolean {
+        const playAgainInput = readlineSync.question('\n니가 이김, 게임 다시할꺼? 할꺼면 1 입력, 안할꺼면 2입력 : ');
         if (playAgainInput === '1') {
-            console.log('게임을 다시 시작합니다.');
+            console.log('\n게임을 다시 시작합니다.');
             this.computerNumber = this.generateThreeRandomNumber();
             return true;
         } else {
@@ -43,7 +45,7 @@ export class App {
         }
     }
 
-    private generateThreeRandomNumber() {
+    private generateThreeRandomNumber(): number {
         const numberRule = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         const shuffleNumbers = this.shuffleNumber(numberRule).slice(0, 3);
         return parseInt(shuffleNumbers.join(''));
@@ -72,7 +74,7 @@ export class App {
         if (uniqueNumber.size !== 3) throw new Error('중복된 숫자가 있습니다. 서로 다른 숫자를 입력하세요.');
     }
 
-    private resultGameValue(userInput: string, computerInput: string) {
+    private resultGameValue(userInput: string, computerInput: string): string {
         let ball;
         let strike = 0;
         let count = 0;
